926e20bc0e341a17156c3e074124e4f46dd84d8e
Mohit-Rawat-7 <mohit.rawat.btech2023@sitpune.edu.in>
2024-09-15 22:12:54 +0530

    Create default.arg.cons.cpp

---
+++ default.arg.cons.cpp
@@ -0,0 +1,21 @@
+//Mohit Rawat
+//23070123086
+#include <iostream>
+using namespace std;
+class Myclass {
+    private:
+    int value1,value2;
+    public:
+    Myclass(int val1=0, int val2=0) {
+        value1=val1;
+        value2=val2;
+        cout<<"Constructor called! Your entered value is: "<<value1<<" and "<<value2 <<endl;
+    }   
+};
+int main() {
+   
+   Myclass obj1; 
+   Myclass obj2(7);
+   Myclass obj3(3,5);
+   return 0;
+}
