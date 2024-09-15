d9a28458f23d85301fdf54affb1c4456d497788c
Mohit-Rawat-7 <mohit.rawat.btech2023@sitpune.edu.in>
2024-09-15 22:18:21 +0530

    Create default.cons.cpp

---
+++ default.cons.cpp
@@ -0,0 +1,15 @@
+//sharvari murade
+//23070123088
+#include <iostream>
+using namespace std;
+class Mohit
+{
+    public:
+    Mohit() {
+        cout<<"Hello Mohit"<<endl;
+    }
+};
+int main() {
+    Mohit obj; 
+    return 0;
+}
