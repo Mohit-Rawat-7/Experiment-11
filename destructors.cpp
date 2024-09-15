0d2b2b8ed5d80b8da9c7141dd79d2039ea04c074
Mohit-Rawat-7 <mohit.rawat.btech2023@sitpune.edu.in>
2024-09-15 22:22:27 +0530

    Create destructors.cpp

---
+++ destructors.cpp
@@ -0,0 +1,18 @@
+//Mohit Rawat
+//23070123086
+#include <iostream>
+using namespace std;
+class MyClass {
+public:
+~MyClass() {
+        cout << "Destructor called!" << endl;
+    }
+MyClass() {
+        cout << "Constructor called!" << endl;
+    }
+};
+int main() {
+    MyClass obj;  
+
+return 0;
+}
